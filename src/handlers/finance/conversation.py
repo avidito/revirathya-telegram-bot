a0ea754ce9e6b8fb560d9a697574d43afa12e8c4
avidito <av.leonardif@gmail.com>
2024-05-08 08:46:01 +0700

    ffeat: fix templating, add ExpenseCreate

--- src/handlers/finance/conversation.py
+++ src/handlers/finance/conversation.py
@@ -21,6 +21,10 @@ from src.components import calendar, numpad
 from src.helpers.bot import BotReplyMarkupHelper
 from src.modules.bootstrap import Usecase
 
+from src.handlers.finance.schemas import (
+    ExpenseCreate,
+)
+
 
 # Conversation
 class FinanceConversation:
@@ -63,44 +67,44 @@ class FinanceConversation:
     def get_conversation(self) -> ConversationHandler:
         # Define Flow
         conv_handler = ConversationHandler(
-            entry_points=[
+            entry_points = [
                 CommandHandler("finance", self.__start),
             ],
             states={
-                FinanceConversation.STATES["INPUT_ACTION"]: [
+                self.STATES["INPUT_ACTION"]: [
                     CallbackQueryHandler(self.__input_date)
                 ],
-                FinanceConversation.STATES["INPUT_DATE"]: [
-                    CallbackQueryHandler(self.__input_budget_group, pattern="^calendar-date="),
+                self.STATES["INPUT_DATE"]: [
+                    CallbackQueryHandler(self.__input_budget_group, pattern = "^calendar-date="),
                     self.__calendar_c.create_conversation(
-                        pattern="gen-calendar",
-                        control_state=FinanceConversation.STATES["CALENDAR_CONTROL"],
-                        end_state=FinanceConversation.STATES["CALENDAR_END_CONTROL"],
-                        after_conv_state=FinanceConversation.STATES["INPUT_DATE"],
-                        fallback_func=self.cancel,
+                        pattern = "gen-calendar",
+                        control_state = self.STATES["CALENDAR_CONTROL"],
+                        end_state = self.STATES["CALENDAR_END_CONTROL"],
+                        after_conv_state = self.STATES["INPUT_DATE"],
+                        fallback_func = self.cancel,
                     )
                 ],
-                FinanceConversation.STATES["INPUT_BUDGET_GROUP"]: [
-                    CallbackQueryHandler(self.__input_budget_type, pattern="^budget-group=")
+                self.STATES["INPUT_BUDGET_GROUP"]: [
+                    CallbackQueryHandler(self.__input_budget_type, pattern = "^budget-group=")
                 ],
-                FinanceConversation.STATES["INPUT_BUDGET_TYPE"]: [
-                    CallbackQueryHandler(self.__input_description, pattern="^budget-type=")
+                self.STATES["INPUT_BUDGET_TYPE"]: [
+                    CallbackQueryHandler(self.__input_description, pattern = "^budget-type=")
                 ],
-                FinanceConversation.STATES["INPUT_DESCRIPTION"]: [
+                self.STATES["INPUT_DESCRIPTION"]: [
                     MessageHandler(filters.Regex(".*"), self.__input_amount)
                 ],
-                FinanceConversation.STATES["INPUT_AMOUNT"]: [
-                    CallbackQueryHandler(self.__input_confirmation, pattern="^numpad-amount="),
+                self.STATES["INPUT_AMOUNT"]: [
+                    CallbackQueryHandler(self.__input_confirmation, pattern = "^numpad-amount="),
                     self.__numpad_c.create_conversation(
-                        pattern="gen-numpad",
-                        control_state=FinanceConversation.STATES["NUMPAD_CONTROL"],
-                        end_state=FinanceConversation.STATES["NUMPAD_END_CONTROL"],
-                        after_conv_state=FinanceConversation.STATES["INPUT_AMOUNT"],
-                        fallback_func=self.cancel,
+                        pattern = "gen-numpad",
+                        control_state = self.STATES["NUMPAD_CONTROL"],
+                        end_state = self.STATES["NUMPAD_END_CONTROL"],
+                        after_conv_state = self.STATES["INPUT_AMOUNT"],
+                        fallback_func = self.cancel,
                     )
                 ],
-                FinanceConversation.STATES["INPUT_CONFIRMATION"]: [
-                    CallbackQueryHandler(self.__echo, pattern="^confirm=")
+                self.STATES["INPUT_CONFIRMATION"]: [
+                    CallbackQueryHandler(self.__echo, pattern = "^confirm=")
                 ],
             },
             fallbacks=[
@@ -115,46 +119,50 @@ class FinanceConversation:
         """
         Starting Finance conversation
         """
-        inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Create", callback_data="command=create")],
+        # Initiate Object
+        if "data" in context.user_data:
+            del context.user_data["data"]
+        context.user_data["data"] = ExpenseCreate()
+        
+        # Reply Chat
+        text = self.__reply_h.render_text(self.__template_dir, "init_conv")
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Create", callback_data = "command=create")],
         ])
-
         await update.message.reply_text(
-            text = self.__reply_h.render_text(self.__template_dir, "base_init"),
-            parse_mode=ParseMode.HTML,
-            reply_markup=inline_keyboard,
+            text = text,
+            parse_mode = ParseMode.HTML,
+            reply_markup = reply_markup,
         )
-        return FinanceConversation.STATES["INPUT_ACTION"]
+
+        # Change State
+        return self.STATES["INPUT_ACTION"]
 
 
     async def __input_date(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # Answer Callback
         query = update.callback_query
-        
-        command = query.data.split("=")[1]
-        context.user_data["command"] = command
-        context.user_data["data"] = []
-
-        # TMP
-        inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Today", callback_data=f"calendar-date={datetime.now(self.__tz).strftime('%Y-%m-%d')}")],
-            [InlineKeyboardButton("Choose Date", callback_data=f"gen-calendar")]
-        ])
-        
         await query.answer()
+        
+        # Parse Callback
+        _, command = query.data.split("=")
+        context.user_data["command"] = command
+
+        # Change Reply Text
+        text_params = {"params": "date"}
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_query_conv", text_params)
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Today", callback_data = f"calendar-date={datetime.now(self.__tz).strftime('%Y-%m-%d')}")],
+            [InlineKeyboardButton("Choose Date", callback_data = f"gen-calendar")]
+        ])
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input",
-                {
-                    **context.user_data,
-                    "params": "date",
-                }
-            ),
+            text=text,
             parse_mode=ParseMode.HTML,
-            reply_markup=inline_keyboard
+            reply_markup=reply_markup
         )
 
-        return FinanceConversation.STATES["INPUT_DATE"]
+        # Change State
+        return self.STATES["INPUT_DATE"]
 
 
     async def __input_budget_group(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -163,30 +171,32 @@ class FinanceConversation:
         await query.answer()
 
         # Parse Callback
-        date_str = query.data.split("=")[1]
-        context.user_data["data"].append({"key": "date", "label": "Date", "value": date_str})
+        _, _date = query.data.split("=")
+        expense_data: ExpenseCreate = context.user_data["data"]
+        expense_data.date = _date
+        context.user_data["data"] = expense_data
 
-        # Get Dim Data
+        # Change Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+            "params": "budget group"
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_query_conv", text_params)
+        
         budget_groups = await self.__usecase.budget_usecase.get_groups()
-        inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(bg.budget_group, callback_data=f"budget-group={bg.id};'{bg.budget_group}'")]
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton(bg.budget_group, callback_data = f"budget-group={bg.id};{bg.budget_group}")]
             for bg in budget_groups
         ])
 
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_exists",
-                {
-                    **context.user_data,
-                    "params": "budget group",
-                }
-            ),
+            text = text,
             parse_mode = ParseMode.HTML,
-            reply_markup = inline_keyboard
+            reply_markup = reply_markup
         )
 
-        return FinanceConversation.STATES["INPUT_BUDGET_GROUP"]
+        # Change State
+        return self.STATES["INPUT_BUDGET_GROUP"]
     
 
     async def __input_budget_type(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -195,33 +205,31 @@ class FinanceConversation:
         await query.answer()
         
         # Parse Callback
-        budget_group_callback = query.data.split("=")[1]
-        budget_group_id, budget_group = budget_group_callback.split(";")
-        budget_group_id = int(budget_group_id)
-        budget_group = budget_group.replace("'", "")
-        context.user_data["data"].append({"key": "budget-group", "label": "Budget Group", "value": budget_group, "id": budget_group_id})
-        
-        # Get Dim Data
-        budget_types = await self.__usecase.budget_usecase.get_types_by_group(budget_group_id)
-        inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(bt.budget_type, callback_data=f"budget-type={bt.id};'{bt.budget_type}'")]
+        _, budget_groups = query.data.split("=")
+        expense_data: ExpenseCreate = context.user_data["data"]
+        expense_data.budget_group = budget_groups
+        context.user_data["data"] = expense_data
+               
+        # Change Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+            "params": "budget type"
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_query_conv", text_params)
+
+        budget_types = await self.__usecase.budget_usecase.get_types_by_group(budget_group_id = expense_data.get_budget_group_id())
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton(bt.budget_type, callback_data = f"budget-type={bt.id};{bt.budget_type}")]
             for bt in budget_types
         ])
 
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_exists",
-                {
-                    **context.user_data,
-                    "params": "budget type",
-                }
-            ),
+            text = text,
             parse_mode = ParseMode.HTML,
-            reply_markup = inline_keyboard
+            reply_markup = reply_markup
         )
 
-        return FinanceConversation.STATES["INPUT_BUDGET_TYPE"]
+        return self.STATES["INPUT_BUDGET_TYPE"]
 
 
     async def __input_description(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -230,103 +238,120 @@ class FinanceConversation:
         await query.answer()
         
         # Parse Callback
-        budget_type_callback = query.data.split("=")[1]
-        budget_type_id, budget_type = budget_type_callback.split(";")
-        budget_type_id = int(budget_type_id)
-        budget_type = budget_type.replace("'", "")
-        context.user_data["data"].append({"key": "budget-type", "label": "Budget Type", "id": budget_type_id, "value": budget_type})
+        _, budget_type = query.data.split("=")
+        expense_data: ExpenseCreate = context.user_data["data"]
+        expense_data.budget_type = budget_type
+        context.user_data["data"] = expense_data
+
+        # Change Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+            "params": "description"
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_query_conv", text_params)
 
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_exists",
-                {
-                    **context.user_data,
-                    "params": "description",
-                }
-            ),
+            text = text,
             parse_mode = ParseMode.HTML,
         )
 
+        # Save Chat ID and Message ID for future edit
         context.user_data["chat_id"] = update.effective_chat.id
         context.user_data["last_message_id"] = query.message.message_id
-        return FinanceConversation.STATES["INPUT_DESCRIPTION"]
+
+        # Change State
+        return self.STATES["INPUT_DESCRIPTION"]
     
 
     async def __input_amount(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # Get Message
         description = update.message.text
-        context.user_data["data"].append({"key": "description", "label": "Description", "value": description})
+        expense_data: ExpenseCreate = context.user_data["data"]
+        expense_data.description = description
+        context.user_data["data"] = expense_data
 
-        # TMP
-        inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Custom Amount", callback_data=f"gen-numpad")],
+        # Edit Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+            "params": "amount"
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_query_conv", text_params)
+
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Custom Amount", callback_data = f"gen-numpad")],
         ])
 
         await context.bot.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_exists",
-                {
-                    **context.user_data,
-                    "params": "amount",
-                }
-            ),
+            text = text,
             parse_mode = ParseMode.HTML,
-            reply_markup = inline_keyboard,
+            reply_markup = reply_markup,
             chat_id = context.user_data["chat_id"],
             message_id = context.user_data["last_message_id"],
         )
 
-        return FinanceConversation.STATES["INPUT_AMOUNT"]
+        # Change State  
+        return self.STATES["INPUT_AMOUNT"]
     
 
     async def __input_confirmation(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # Answer Callback
         query = update.callback_query
+        await query.answer()
 
-        amount = query.data.split("=")[1]
-        context.user_data["data"].append({"key": "amount", "label": "Amount", "value": amount})
+        # Parse Callback
+        _, amount = query.data.split("=")
+        expense_data: ExpenseCreate = context.user_data["data"]
+        expense_data.amount = amount
+        context.user_data["data"] = expense_data
         
-        inline_keyboard = InlineKeyboardMarkup([
+        # Edit Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_confirm_conv", text_params)
+
+        reply_markup = InlineKeyboardMarkup([
             [
                 InlineKeyboardButton("Yes", callback_data=f"confirm=yes"),
                 InlineKeyboardButton("No", callback_data=f"confirm=no")
             ]
         ])
 
-        await query.answer()
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_confirmation",
-                {
-                    **context.user_data,
-                }
-            ),
+            text = text,
             parse_mode = ParseMode.HTML,
-            reply_markup = inline_keyboard
+            reply_markup = reply_markup,
         )
 
-        return FinanceConversation.STATES["INPUT_CONFIRMATION"]
+        # Change State
+        return self.STATES["INPUT_CONFIRMATION"]
     
 
     async def __echo(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+        # Answer Callback
         query = update.callback_query
-        
-        confirm = query.data.split("=")[1]
-        context.user_data["confirm"] = confirm
-
         await query.answer()
+        
+        # Parse Callback
+        _, confirm = query.data.split("=")
+        context.user_data["confirm"] = confirm
+        expense_data: ExpenseCreate = context.user_data["data"]
+
+        # Edit Reply Text
+        text_params = {
+            "data": expense_data.to_message(),
+            "confirm": confirm,
+        }
+        text = self.__reply_h.render_text(self.__template_dir, "create_input_confirm_conv", text_params)
+        
         await query.edit_message_text(
-            text = self.__reply_h.render_text(
-                self.__template_dir,
-                "base_input_confirmation",
-                {
-                    **context.user_data,
-                }
-            ),
-            parse_mode=ParseMode.HTML,
+            text = text,
+            parse_mode = ParseMode.HTML,
         )
 
+        # Change State
+        del context.user_data["data"]
+        del context.user_data["confirm"]
         return ConversationHandler.END
 
 
